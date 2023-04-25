477b21923e23417cb50956f7cdc897549528fbee
Esteban Borai <estebanborai@gmail.com>
2023-04-25 00:06:17 -0400

    feat: add version

--- src/certificate.rs
+++ src/certificate.rs
@@ -1,11 +1,14 @@
 use std::fmt::Display;
 
+use x509_parser::x509::X509Version;
+
 #[derive(Clone, Debug)]
 pub struct Certificate {
     pub issuer: String,
     pub subject: String,
     pub val_not_before: String,
     pub val_not_after: String,
+    pub version: X509Version,
 }
 
 impl Certificate {
@@ -13,18 +16,18 @@ impl Certificate {
         use x509_parser::prelude::*;
 
         if let Ok((_, cert)) = X509Certificate::from_der(bytes) {
-            let issuer = cert.issuer.to_string();
-            let subject = cert.subject.to_string();
             let validity = cert.validity.clone();
             let val_not_before = validity.not_before.to_datetime().to_string();
             let val_not_after = validity.not_after.to_datetime().to_string();
-
-            return Self {
-                issuer,
-                subject,
+            let certificate = Certificate {
+                issuer: cert.issuer().to_string(),
+                subject: cert.subject().to_string(),
                 val_not_after,
                 val_not_before,
+                version: cert.version(),
             };
+
+            return certificate;
         }
 
         panic!("Failed to parse certificate");
@@ -35,8 +38,8 @@ impl Display for Certificate {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "Issuer: {}\nSubject: {}\nNot Before: {}\nNot After: {}",
-            self.issuer, self.subject, self.val_not_before, self.val_not_after
+            "Issuer: {}\nSubject: {}\nNot Before: {}\nNot After: {}\nVersion: {}",
+            self.issuer, self.subject, self.val_not_before, self.val_not_after, self.version
         )
     }
 }
